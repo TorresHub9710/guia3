4992769034fff9d88a7644136f61a534770dd1fa
TorresHub9710 <heortiz34510@universidadean.edu.co>
2022-12-03 16:25:40 -0500

    eliminación archivos viejos

--- JS/tips.js
+++
@@ -1,8 +0,0 @@
-//--------------Funcionalidad Menu-------------------//
-document.getElementById("icono-menu").addEventListener("click",mostrar_menu);
-
-function mostrar_menu() {
-
-    document.getElementById("move-contenido").classList.toggle('move-contenedor-principal');
-    document.getElementById("mostrar-menu").classList.toggle('mostrar-menu-lateral');
-}
